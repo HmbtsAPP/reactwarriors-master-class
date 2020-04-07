27dd6d9a699ff8fce230b6c6943de472ec07cf35
HmbtS <alex@arsyonov.org.ua>
2020-04-07 13:43:36 +0300

    Day 4.2

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -4,8 +4,14 @@ const MovieItem = (props) => {
   console.log(props);
   const { movie, removeMovie } = props;
   return (
-    <div>
-      <p>{movie.title}</p>
+    <div className="card-body">
+      <h6 className="card-title">{movie.title}</h6>
+      <div className="d-flex justify-content-between align-items-center">
+        <p className="mb-0">Rating: {movie.vote_average}</p>
+        <button type="button" className="btn btn-secondary">
+          Will Watch
+        </button>
+      </div>
       <button onClick={removeMovie.bind(null, movie)}>Delete movie</button>
     </div>
   );
