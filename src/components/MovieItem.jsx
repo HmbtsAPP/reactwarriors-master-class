1f9a22d89990e4cdc6f4dd5a576dcfc19a93af78
HmbtS <alex@arsyonov.org.ua>
2020-04-07 13:37:59 +0300

    Day 4.1

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const MovieItem = (props) => {
   console.log(props);
+  const { movie, removeMovie } = props;
   return (
     <div>
-      <p>{props.movie.title}</p>
-      <button onClick={props.removeMovie.bind(null, props.movie)}>
-        Delete movie
-      </button>
+      <p>{movie.title}</p>
+      <button onClick={removeMovie.bind(null, movie)}>Delete movie</button>
     </div>
   );
 };
