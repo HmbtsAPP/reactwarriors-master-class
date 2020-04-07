e77c7c257b8e3f16e9fa128136ca6697cc468149
HmbtS <alex@arsyonov.org.ua>
2020-04-07 13:49:02 +0300

    Day4.3

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -4,15 +4,24 @@ const MovieItem = (props) => {
   console.log(props);
   const { movie, removeMovie } = props;
   return (
-    <div className="card-body">
-      <h6 className="card-title">{movie.title}</h6>
-      <div className="d-flex justify-content-between align-items-center">
-        <p className="mb-0">Rating: {movie.vote_average}</p>
-        <button type="button" className="btn btn-secondary">
-          Will Watch
-        </button>
+    <div className="card">
+      <img
+        className="card-img-top"
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.item.backdrop_path || movie.poster_path
+        }`}
+        alt=""
+      />
+      <div className="card-body">
+        <h6 className="card-title">{movie.title}</h6>
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="mb-0">Rating: {movie.vote_average}</p>
+          <button type="button" className="btn btn-secondary">
+            Will Watch
+          </button>
+        </div>
+        <button onClick={removeMovie.bind(null, movie)}>Delete movie</button>
       </div>
-      <button onClick={removeMovie.bind(null, movie)}>Delete movie</button>
     </div>
   );
 };
--- src/templates.js
+++ src/templates.js
@@ -6,7 +6,15 @@
 //       data.poster_path}`}
 //     alt=""
 //   />
-
+//   <div className="card-body">
+//     <h6 className="card-title">{data.title}</h6>
+//     <div className="d-flex justify-content-between align-items-center">
+//       <p className="mb-0">Rating: {data.vote_average}</p>
+//       <button type="button" className="btn btn-secondary">
+//         Will Watch
+//       </button>
+//     </div>
+//   </div>
 // </div>;
 // MovieListWillWatch
 // <h4>Will Watch: movies</h4>
